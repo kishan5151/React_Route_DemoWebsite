fb6e704b6ea3583fb69e1c8684dc61d684773146
kishan rana <144171885+kishan5151@users.noreply.github.com>
2024-02-23 01:13:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App=()=>{
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/services/" element={<Services/>}/>
+        <Route path="/services" element={<Services/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
       </Routes>
